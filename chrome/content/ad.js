5bf665574531d9c9d5f576fad0cb154fc2f1adfb
jordi <beltrachi@gmail.com>
2009-03-15 20:16:12 +0100

    source formatted

--- chrome/content/ad.js
+++ chrome/content/ad.js
@@ -1,20 +1,21 @@
-
-
 /** 
  TODO:
   - Detect when a recipient has been added
   - Fetch if this address has a defined language
   - Change the language of the spellchecker to the guessed one
 */
+
 var automatic_dictionary_class = function(){};
 automatic_dictionary_class.prototype = {
   
   ADDRESS_INFO_PREF:"extensions.automatic_dictionary.addressesInfo",
+  forced_lang: false,
   
   init: function(){
     this.prefManager = Components.classes["@mozilla.org/preferences-service;1"]
                                 .getService(Components.interfaces.nsIPrefBranch);
-    setTimeout( "automatic_dictionary.loadData();",0 );
+    //Watch values!
+    setTimeout( "automatic_dictionary.loadData();",0 ); //Deferred
   },
   
   loadData: function(){
@@ -23,9 +24,9 @@ automatic_dictionary_class.prototype = {
       eval("this.data = ("+ value+ ")");
     }catch( e ){
       //TODO: what??
-      alert(e.toString());
+      alert(e.toString()); //FIXME
     }
-    this.saveData();
+    this.saveData(); //Fixme!
   },
   
   saveData: function(){
@@ -40,6 +41,4 @@ automatic_dictionary_class.prototype = {
 }
 var automatic_dictionary = new automatic_dictionary_class();
 automatic_dictionary.init();
-
-//setTimeout(function(){alert("foo");},0);
 
