6cfff2117a7ac15afb0a2f6709da2d6cc3981d47
beltrachi <beltrachi@gmail.com>
2010-03-27 20:20:57 +0100

    Unsingletonized class and some concurrency issues fixed

--- chrome/content/ad.js
+++ chrome/content/ad.js
@@ -9,9 +9,162 @@
   - Fetch if this address has a defined language
   - Change the language of the spellchecker to the saved one
 */
-//Only load if its not already loaded.
-automatic_dictionary = {
-  
+
+/**
+    Implementation comments:
+    
+     * To support various compose windows we share the data hash between instances
+       of ad, through the preferences service with SharedHash
+*/
+var AutomaticDictionary = {};
+/**
+ *   SharedHash is used to share preferences between compose windows in case you
+ *   have more than one window open.
+ *   Features:
+ *       * When a set is done, it will be available to all SharedHashes through preferences-service
+ *       * To spread the changes we save a version number in another preference field
+ */
+AutomaticDictionary.dump = function(msg){
+    dump("AutomaticDictionary: ");
+    dump(msg);
+    dump("\n");
+}
+AutomaticDictionary.SharedHash = function( prefPath ){
+    this.prefPath = prefPath;
+    this.lockPath = prefPath + ".lock";
+    this.prefManager = Components.classes["@mozilla.org/preferences-service;1"]
+                                        .getService(Components.interfaces.nsIPrefBranch);
+    this.id = (new Date()).getTime().toString() + Math.floor(Math.random()*1000000); //Taking into account that we will not init two SharedHashes at once
+    
+    return this; //Needed?
+}
+AutomaticDictionary.SharedHash.prototype = {
+    id: null,
+    internal_version: 1,
+    prefPath: null,
+    prefManager: null,
+    lockPath: null,
+    data: null,
+    version: null,
+
+    load: function(){
+        this.data = this.readData();
+        this.version = this.readVersion();
+    },
+    readVersion: function(){
+        return this.prefManager.getCharPref( this.prefPath + ".version" );
+    },
+    writeVersion: function( v ){
+        return this.prefManager.setCharPref( this.prefPath + ".version", v);
+    },
+    readData:function(){
+        var v = this.prefManager.getCharPref( this.prefPath );
+        if( v ){
+            try{
+                return JSON.parse( v );
+            }catch(e){
+                return {};
+            }
+        }else
+            return {};
+    },
+    writeData:function(){
+        return this.prefManager.setCharPref( this.prefPath, JSON.stringify( this.data ) );
+    },
+    
+    sincronized:function( func, force ){
+        if( force ) this.log("WARN AutomaticDictionary.SharedHash#sincronized with force");
+        try{
+            this.log("lock is " + JSON.stringify(this.prefManager.getCharPref( this.lockPath )));
+            var nestedSync = this.gotLock();
+            if( force || this.prefManager.getCharPref( this.lockPath ).length == 0 || nestedSync ){
+                this.prefManager.setCharPref( this.lockPath, this.id );
+                this.log("2 - lock is " + JSON.stringify(this.prefManager.getCharPref( this.lockPath )));
+                if( force || this.gotLock() ){
+                    this.log("executing on sync");
+                    func();
+                    if( !nestedSync ){
+                        this.prefManager.setCharPref( this.lockPath, "" ); //Release lock
+                        this.log("lock released is " + this.prefManager.getCharPref( this.lockPath ));
+                    }else{
+                        this.log("nested lock not released");
+                    }
+                    return true;
+                }else{
+                    this.log("lock is not its own: " + this.prefManager.getCharPref( this.lockPath ) + " != " + this.id);
+                }
+            }else{
+                this.log("no force neither is empty");
+            }
+        }catch(e){
+            this.log("ERROR Sincronized failed: " + e.message);
+            this.log("INFO Releasing the lock");
+            this.prefManager.setCharPref( this.lockPath, "" ); //Release the lock
+            throw e;
+        }
+        this.log("syncronized is false");
+        return false;
+    },
+    
+    gotLock:function(){
+        return this.prefManager.getCharPref( this.lockPath ).toString() == this.id;
+    },
+    
+    /**
+    * Function to try to do an operation in sync but gets tired after n retrires
+    */
+    sincronized_with_patience:function( func, times ){
+        for(var t=0; t<times; t++){
+            if(this.sincronized(func, t == (times-1))) return true;
+            this.log("DEBUG AD.SharedHash#sincronized_with_patience it:"+t);
+        }
+        this.log("UNREACHABLE CODE REACHED. Internal error. Logs and debugging can be required.");
+        return false; //It should not reach here never!
+    },
+    
+    set: function(key, value){
+        var next_version = this.id + this.internal_version++;
+        var _this = this;
+        this.sincronized_with_patience( 
+            function(){
+                _this.refresh(); //Avoid to ask for sync again
+                _this.data[key] = value;
+                _this.writeVersion( next_version );
+                _this.writeData();
+                _this.log("INFO AutomaticDictionary.SharedHash("+_this.id+") write data sucess");
+            }, 10 );
+    },
+    
+    get: function(key){
+        this.refresh();
+        return this.data[key];
+    },
+    //Updates to last version of data
+    refresh:function(){
+        if( this.version != this.readVersion() ){
+            var _this = this;
+            this.sincronized_with_patience(function(){ _this.load() }, 10 );
+        }
+    },
+    log: function(msg){
+        AutomaticDictionary.dump(msg);
+    }
+}
+
+AutomaticDictionary.Class = function(){
+    this.log("ad: init");
+    this.running = true;
+    this.prefManager = Components.classes["@mozilla.org/preferences-service;1"]
+                                .getService(Components.interfaces.nsIPrefBranch);
+    this.iter = 0; //ObserveRecipients execution counter
+    this.data = new AutomaticDictionary.SharedHash( this.ADDRESS_INFO_PREF );
+    this.setListeners();
+    this.initialized = true;
+    this.start();
+    return this;
+}
+
+AutomaticDictionary.Class.prototype = {
   //Constants
   ADDRESS_INFO_PREF:"extensions.automatic_dictionary.addressesInfo",
   POLLING_DELAY: 3000, //Miliseconds
@@ -21,24 +174,9 @@ automatic_dictionary = {
   last_language_set: null,
   initialized: false,
   running: false, //Stopped
-  data: {},
+  shared: null,
   last_timeout: null, //Timer object of the next poll
-  
-  init: function(){
-    this.log("ad: init");
-    this.running = true;
-    if( !this.initalized ){
-        this.prefManager = Components.classes["@mozilla.org/preferences-service;1"]
-                                    .getService(Components.interfaces.nsIPrefBranch);
-        this.iter = 0; //ObserveRecipients execution counter
-        setTimeout( "automatic_dictionary.loadData(true);",10 ); //Deferred
-        this.initialized = true;
-    }else{
-        //already initialized
-        this.observeRecipients();
-    }
-    this.setListeners();
-  },
+  instance_number: -1,
   
   stop: function(){
     this.log("ad: stop");
@@ -47,31 +185,10 @@ automatic_dictionary = {
     this.last_timeout = null;
   },
   
-  loadData: function( initPolling ){
-    var value = this.prefManager.getCharPref( this.ADDRESS_INFO_PREF );
-    try{
-        if( !value ){
-            value = "{}";
-        }
-        eval("automatic_dictionary.data = ("+ value + ")");
-    }catch( e ){
-      //TODO: what??
-      this.log(e.toString()); //FIXME
-      throw e;
-    }
-    //After data loaded, observe the adresses list to update lang if required
-    if( initPolling ) this.observeRecipients();
-  },
-  
-  saveData: function(){
-    var value = "{}";
-    if( this.data && this.data.toSource ){
-      value = this.data.toSource();
-    }else{
-      //TODO: something happened!! what to do?
-      this.log(this.data);
-    }
-    this.prefManager.setCharPref( this.ADDRESS_INFO_PREF, value );
+  start: function(){
+    this.log("ad: start");
+    this.running = true;
+    this.observeRecipients();
   },
   
   observeRecipients: function(){
@@ -83,10 +200,13 @@ automatic_dictionary = {
       this.detectUserOverridenLanguage();
       this.deduceLanguage();
       //Queue next call
-      if( this.running )
-          this.last_timeout = setTimeout("automatic_dictionary.observeRecipients();", this.POLLING_DELAY );
+      if( this.running ){
+          var _this = this;
+          this.last_timeout = setTimeout(function(){_this.observeRecipients();}, this.POLLING_DELAY );
+      }
     }catch(e){
       this.changeLabel( e.toString());
+      throw e;
     }
   },
   
@@ -101,12 +221,11 @@ automatic_dictionary = {
       //The user has set the language for the recipients
       //We update the assignment of language for those recipients
       for( var i in arr){
-        this.data[arr[i]] = current_lang;
+        this.data.set(arr[i], current_lang);
       }
-      this.saveData();
       this.user_overriden_lang = true;
       this.last_language_set = current_lang;
-      this.changeLabel( " Saved " + current_lang + " as default for " + arr.length + " recipients" );
+      this.changeLabel( "Saved " + current_lang + " as default for " + arr.length + " recipients" );
     }
     //When user removes recipients, language detection starts again
     if(arr.length == 0){
@@ -133,6 +252,7 @@ automatic_dictionary = {
         worked = true;
       }catch( e ){
         this.changeLabel( "Error: Could not set lang to "+ target_lang+ ". Maybe its not installed any more?" );
+        throw e;
       }
       if(worked) this.changeLabel( "Deduced " + target_lang );
     }
@@ -145,7 +265,7 @@ automatic_dictionary = {
   },
   
   getLangFor: function( addr ){
-    return this.data[addr];
+    return this.shared.data.get(addr);
   },
   
   getRecipients: function(){
@@ -166,16 +286,16 @@ automatic_dictionary = {
   },
   
   setListeners: function(){
-      //var w = document.getElementById("msgcomposeWindow");
-      //this.log( " w is " + w.toSource());
-      if( window ){
-          window.addEventListener("compose-window-close", function(){automatic_dictionary.stop()}, true);
-          this.log("event seem to be registered");
-      }else{
-          this.changeLabel("Internal error (Init. listeners)");
-          this.log("no window found");
-      }
+    if( window ){
+        var _this = this;
+        window.addEventListener("compose-window-close", function(){_this.stop()}, true);
+        this.log("event seem to be registered");
+    }else{
+        this.changeLabel("Internal error (Init. listeners)");
+        this.log("no window found");
+    }
   },
+
   
   getLabel: function(){
       return document.getElementById("automatic-dictionary-panel");
@@ -190,8 +310,9 @@ automatic_dictionary = {
   },
   
   log:function( msg ){
-      window.dump( "automatic_dictionary: " + msg + "\n");
+      AutomaticDictionary.dump( msg );
   }
 }
-automatic_dictionary.init();
+
+var automatic_dictionary_instance = new AutomaticDictionary.Class();
 
--- defaults/preferences/ad.js
+++ defaults/preferences/ad.js
@@ -1,4 +1,6 @@
 //addressesInfo contains the history for each address to know the language
 //for each contact
 pref("extensions.automatic_dictionary.addressesInfo", "");
+pref("extensions.automatic_dictionary.addressesInfo.version", "");
+pref("extensions.automatic_dictionary.addressesInfo.lock", "");
 
