19f22bf1ea93dfaa71f8229d8c3b5f2729f636ef
beltrachi <beltrachi@gmail.com>
2010-08-28 15:17:25 +0200

    fixing build script to include locales

--- config_build.sh
+++ config_build.sh
@@ -3,7 +3,7 @@
 # Build config for the build script, build.sh. Look there for more info.
 
 APP_NAME=automatic_dictionary          # short-name, jar and xpi files name. Must be lowercase with no spaces
-CHROME_PROVIDERS="chrome/content"  # which chrome providers we have (space-separated list)
+CHROME_PROVIDERS="chrome/content chrome/locale"  # which chrome providers we have (space-separated list)
 CLEAN_UP=1          # delete the jar / "files" when done?       (1/0)
 ROOT_FILES="README"        # put these files in root of xpi (space separated list of leaf filenames)
 ROOT_DIRS="defaults"         # ...and these directories       (space separated list)
