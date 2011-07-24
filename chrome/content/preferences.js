90c97b362e431fd1d64eb0161e9f3c55af0d1f55
Jordi Beltran <beltrachi@gmail.com>
2011-07-24 23:27:06 +0200

    Bump version compatibility to Thunderbird 5.x

---
+++ chrome/content/preferences.js
@@ -0,0 +1,22 @@
+if( typeof(AutomaticDictionary)=== "undefined" ){
+    var AutomaticDictionary = {};
+}
+
+//Used on a text field allows to control the value of the field in a range.
+// defv is the default value
+AutomaticDictionary.check_int_range = function( field , min, max, defv ){
+    defv = defv || min;
+    var value = field.value * 1;
+    // reset if it's not the same as string of it 
+    if( (value === NaN) || (value + "" !== field.value) ){
+        field.value = defv;
+    }
+    if( value < min ){
+        field.value = min;
+    }else if( value > max ){
+        field.value = max;
+    }
+    
+    return false;
+};
+
