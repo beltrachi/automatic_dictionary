7a8bdfb2b47a590eaaaff94f941f996c2c79b355
Jordi Beltran <beltrachi@gmail.com>
2012-11-11 21:18:56 +0100

    Testing performance of freq table and pair counter

---
+++ test/performance/freq_table_test.js
@@ -0,0 +1,24 @@
+var bm_options = {
+    soft:true
+}; 
+
+var amount = 1000;
+
+var obj = new AutomaticDictionary.Lib.FreqTable();
+logger.info("FreqTable test with "+amount+" ops")
+assert.benchmark( 200, function(){
+    var k;
+    for(var i=0; i < amount; i++){
+        //10 diferent first items and 11 second items 
+        obj.add("a"+(i%10));
+    }
+}, bm_options );
+
+assert.benchmark( 200, function(){
+    var k;
+    for(var i=0; i < amount; i++){
+        //10 diferent first items and 11 second items 
+        obj.remove("a"+(i%10));
+    }
+}, bm_options );
+
