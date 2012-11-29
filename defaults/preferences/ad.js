ad7facf4e3c46b523e2c63fd30e6b5e7bb80750b
Jordi Beltran <beltrachi@gmail.com>
2012-11-29 23:02:23 +0100

    Set preferences to be stored between session

--- defaults/preferences/ad.js
+++ defaults/preferences/ad.js
@@ -9,3 +9,16 @@ pref("extensions.automatic_dictionary.migrations_applied", "");
 pref("extensions.automatic_dictionary.maxRecipients", 10);
 pref("extensions.automatic_dictionary.allowCollect", true);
 
+pref("extensions.automatic_dictionary.allowHeuristic", true);
+pref("extensions.automatic_dictionary.freqTableData", "");
+pref("extensions.automatic_dictionary.freqTableData.version", "");
+pref("extensions.automatic_dictionary.freqTableData.lock", "");
+
+pref("extensions.automatic_dictionary.first_visit", "");
+pref("extensions.automatic_dictionary.last_session", "");
+pref("extensions.automatic_dictionary.session_number", "");
+pref("extensions.automatic_dictionary.visitor_id", "");
+
+
+
+
