7ce1d41ded37f42ee32332890250d659d85fcf4d
Jordi Beltran <beltrachi@gmail.com>
2017-08-29 00:22:13 +0200

    Adding install_extension script

---
+++ script/install_extension.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -ex
+
+# Adds an extension to a thunderbird profile
+# Example: install_extension.sh --path /foo --extension file.xpi
+
+# Parse options
+while [[ $# -gt 1 ]]
+do
+    key="$1"
+
+    case $key in
+        -e|--extension)
+            EXTENSION="$2"
+            shift # past argument
+            ;;
+        -p|--path)
+            PROFILE_PATH="$2"
+            shift # past argument
+            ;;
+        *)
+            # unknown option
+            ;;
+    esac
+    shift # past argument or value
+done
+
+if [ -z "$EXTENSION"] || [ -z "$PROFILE_PATH" ]; then
+    echo "Extension and path parameters required."
+    exit 1;
+fi
+
+mkdir -p "$PROFILE_PATH/extensions"
+
+TMP_DIR=$(mktemp -d)
+
+cp $EXTENSION $TMP_DIR
+unzip "$TMP_DIR/$EXTENSION" -d $TMP_DIR
+rm $TMP_DIR/$EXTENSION
+
+EXTENSION_ID=$(cat $TMP_DIR/install.rdf |grep "em:id" | head -n 1 | cut -d ">" -f 2 | cut -d "<" -f 1)
+
+mkdir "$PROFILE_PATH/extensions/$EXTENSION_ID"
+mv $TMP_DIR/* "$PROFILE_PATH/extensions/$EXTENSION_ID"
+
+echo $EXTENSION $PROFILE_PATH
+
+rm -rf $TMP_DIR
+
+echo "Extension $EXTENSION installed correctly"
