b237dd7aa55e60efb62073f9089dd9923acf2902
Jordi Beltran <beltrachi@gmail.com>
2018-04-08 14:11:04 +0200

    Test snapshooter class

--- test/integration/spec/lib/interactor/snapshooter_spec.rb
+++ test/integration/spec/lib/interactor/snapshooter_spec.rb
@@ -1,1 +1,17 @@
 require_relative '../../spec_helper'
+
+require 'interactor'
+
+describe Interactor::Snapshooter do
+  let(:instance) { described_class }
+
+  context '#create_screenshot' do
+    it 'captures with import' do
+      expect(instance).to receive(:run) do |command|
+        expect(command).to start_with('import -window root')
+        expect(command).to include('jpg')
+      end
+      instance.create_screenshot
+    end
+  end
+end
