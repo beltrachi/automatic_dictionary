98babc86a180e0ad7a1e4b661d42eb80b3a9b879
Jordi Beltran <beltrachi@gmail.com>
2020-02-16 22:20:52 +0100

    Update build script for MailExtension

--- update_version.sh
+++ update_version.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
+set -ex
 # Extract version from manifest.json and set to version.js
-VERSION=`ruby -e "require 'json'; puts JSON.load(File.read('manifest.json'))['version']"`
-JS_FILE='chrome/content/version.js'
+VERSION=`ruby -e "require 'json'; puts JSON.load(File.read('addon/manifest.json'))['version']"`
+JS_FILE='addon/version.js'
 TMP_FILE=${JS_FILE}'.tmp'
 cat $JS_FILE | sed -e "s/version='[^']\+/version='${VERSION}/g" > $TMP_FILE
 mv -f $TMP_FILE $JS_FILE
