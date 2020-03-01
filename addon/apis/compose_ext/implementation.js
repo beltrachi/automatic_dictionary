d72f52e0f8ba634a3d54208b78b369c319b781c2
Jordi Beltran <beltrachi@gmail.com>
2020-03-01 16:59:14 +0100

    First api experiment says hello

---
+++ addon/apis/compose_ext/implementation.js
@@ -0,0 +1,95 @@
+/* eslint-disable object-shorthand */
+
+// Get various parts of the WebExtension framework that we need.
+var { ExtensionCommon } = ChromeUtils.import("resource://gre/modules/ExtensionCommon.jsm");
+
+// You probably already know what this does.
+var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
+
+// This is the important part. It implements the functions and events defined in schema.json.
+// The variable must have the same name you've been using so far, "myapi" in this case.
+var compose_ext = class extends ExtensionCommon.ExtensionAPI {
+  getAPI(context) {
+    return {
+      // Again, this key must have the same name.
+      compose_ext: {
+
+        // A function.
+        sayHello: async function(name) {
+          Services.wm.getMostRecentWindow("mail:3pane").alert("Hello " + name + "!");
+        },
+
+        // An event. Most of this is boilerplate you don't need to worry about, just copy it.
+        onLanguageChange: new ExtensionCommon.EventManager({
+          context,
+          name: "myapi.onLanguageChange",
+          // In this function we add listeners for any events we want to listen to, and return a
+          // function that removes those listeners. To have the event fire in your extension,
+          // call fire.async.
+          register(fire) {
+            function callback(event, language) {
+              return fire.async(language);
+            }
+
+            windowListener.add(callback);
+            return function() {
+              windowListener.remove(callback);
+            };
+          },
+        }).api(),
+
+      },
+    };
+  }
+};
+
+// A helpful class for listening to windows opening and closing.
+// (This file had a lowercase E in Thunderbird 65 and earlier.)
+var { ExtensionSupport } = ChromeUtils.import("resource:///modules/ExtensionSupport.jsm");
+
+// This object is just what we're using to listen for toolbar clicks. The implementation isn't
+// what this example is about, but you might be interested as it's a common pattern. We count the
+// number of callbacks waiting for events so that we're only listening if we need to be.
+var windowListener = new class extends ExtensionCommon.EventEmitter {
+  constructor() {
+    super();
+    this.callbackCount = 0;
+  }
+
+  handleEvent(event) {
+    // TODO: fetch which language is it.
+    windowListener.emit("language-changed", 'es');
+  }
+
+  add(callback) {
+    this.on("language-changed", callback);
+    this.callbackCount++;
+
+    if (this.callbackCount == 1) {
+        ExtensionSupport.registerWindowListener("windowListener", {
+            //TODO: set composer url.
+        chromeURLs: ["chrome://messenger/content/messenger.xul"],
+            onLoadWindow: function(window) {
+                //TODO: fetch language changed item and attach to it's event.
+                // let toolbox = window.document.getElementById("mail-toolbox");
+                // toolbox.addEventListener("click", windowListener.handleEvent);
+        },
+      });
+    }
+  }
+
+  remove(callback) {
+    this.off("language-changed", callback);
+    this.callbackCount--;
+
+    if (this.callbackCount == 0) {
+        for (let window of ExtensionSupport.openWindows) {
+            //TODO: remove event listeners from all windows
+            // if (window.location.href == "chrome://messenger/content/messenger.xul") {
+            //   let toolbox = window.document.getElementById("mail-toolbox");
+            // toolbox.removeEventListener("click", this.handleEvent);
+        }
+    }
+    ExtensionSupport.unregisterWindowListener("windowListener");
+  }
+};
--- addon/background.js
+++ addon/background.js
@@ -1,2 +1,6 @@
 // TODO: migrate prefs to local stoarge.
 console.log('Background script loaded.');
+
+console.log("HI HI HI");
+console.log(browser.compose_ext.sayHello('foooofff'));
+console.log("HO HO HO");
