a3f58ac1c8c61f3674d24f63fe08b8138950d0c2
Jordi Beltran <beltrachi@gmail.com>
2020-03-24 21:16:23 +0100

    Fix event dispatcher to be usable in more than one object

--- addon/lib/event_dispatcher.js
+++ addon/lib/event_dispatcher.js
@@ -4,45 +4,49 @@ export function apply(AutomaticDictionary){
  * Usage: AutomaticDictionary.extend( myclass, EventDispatcher );
  **/
 // Based on https://github.com/mrdoob/eventdispatcher.js
-AutomaticDictionary.EventDispatcher = {
-    event_listeners: {},
+  AutomaticDictionary.EventDispatcher = {
+    event_listeners: null,
+    initEventListener: function(){
+      this.event_listeners = this.event_listeners || {};
+    },
     addEventListener: function ( type, listener ) {
-        if ( this.event_listeners[ type ] === undefined ) {
-            this.event_listeners[ type ] = [];
-        }
-        if ( this.event_listeners[ type ].indexOf( listener ) === - 1 ) {
-            this.event_listeners[ type ].push( listener );
-        }
+      this.initEventListener();
+      if ( this.event_listeners[ type ] === undefined ) {
+        this.event_listeners[ type ] = [];
+      }
+      if ( this.event_listeners[ type ].indexOf( listener ) === - 1 ) {
+        this.event_listeners[ type ].push( listener );
+      }
     },
     removeEventListener: function ( type, listener ) {
-        var index = this.event_listeners[ type ].indexOf( listener );
-        if ( index !== - 1 ) {
-            if(this.logger)
-                this.logger.debug("removing eventListener for "+type);
+      this.initEventListener();
+      var index = (this.event_listeners[ type ]|| []).indexOf( listener );
+      if ( index !== - 1 ) {
+        if(this.logger)
+          this.logger.debug("removing eventListener for "+type);
 
-            this.event_listeners[ type ].splice( index, 1 );
-        }
+        this.event_listeners[ type ].splice( index, 1 );
+      }
     },
     dispatchEvent: function ( event ) {
-        if(this.logger) {
-          this.logger.debug("dispatching event ");
-          this.logger.debug(JSON.stringify(event));
+      this.initEventListener();
+      if(this.logger) {
+        this.logger.debug("dispatching event ");
+        this.logger.debug(JSON.stringify(event));
+      }
+      var listenerArray = this.event_listeners[ event.type ] || [];
+      var list;
+      event.target = this;
+      //cloning the listeners in case it removes itself and offsets the
+      //positions so losing a listener call.
+      list = ([]).concat(listenerArray);
+      for ( var i = 0, l = list.length; i < l; i ++ ) {
+        try{
+          list[ i ].call( this, event );
+        }catch(e){
+          AutomaticDictionary.logException(e);
         }
-        var listenerArray = this.event_listeners[ event.type ];
-        var list;
-        if ( listenerArray !== undefined ) {
-            event.target = this;
-            //cloning the listeners in case it removes itself and offsets the
-            //positions so losing a listener call.
-            list = ([]).concat(listenerArray);
-            for ( var i = 0, l = list.length; i < l; i ++ ) {
-                try{
-                    list[ i ].call( this, event );
-                }catch(e){
-                    AutomaticDictionary.logException(e);
-                }
-            }
-        }
+      }
     }
-}
+  }
 }
