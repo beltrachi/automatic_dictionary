165d44ee351b03c881139720d3aa1c58da93f497
Jordi Beltran <beltrachi@gmail.com>
2020-08-22 19:44:35 +0200

    Release 2.0.1

--- addon/version.js
+++ addon/version.js
@@ -1,5 +1,5 @@
 // Do not edit manually.
 // File updated by build.sh
 export function apply(AutomaticDictionary) {
-    AutomaticDictionary.version='2.0.0';
+    AutomaticDictionary.version='2.0.1';
 }
