7bedeb2c04745f28dd5d28bfa88c1aaaafbb07c5
Jordi Beltran <beltrachi@gmail.com>
2020-10-10 21:17:21 +0200

    Fix console error window.gSpellChecker is undefined message
    It was not affecting any feature but was polluting the error console

--- addon/apis/compose_ext/implementation.js
+++ addon/apis/compose_ext/implementation.js
@@ -31,7 +31,7 @@ var compose_ext = class extends ExtensionCommon.ExtensionAPI {
         canSpellCheck: async function(tabId) {
           // todo: use tabId
           var window = await getTabWindow(tabId);
-          return window.gSpellChecker.canSpellCheck && window.gSpellChecker.enabled;
+          return window.gSpellChecker && window.gSpellChecker.canSpellCheck && window.gSpellChecker.enabled;
         },
 
         setSpellCheckerLanguage: async function(tabId, lang) {
