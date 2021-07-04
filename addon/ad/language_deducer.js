1a3c402f4623f9120f413aa1deb1d271fe9f707e
Jordi Beltran <beltrachi@gmail.com>
2021-07-04 15:20:02 +0200

    Improve readability by using named methods

--- addon/ad/language_deducer.js
+++ addon/ad/language_deducer.js
@@ -8,6 +8,14 @@ const Deduction = function(language, method){
 
 export const LanguageDeducer = function(ad){
   this.ad = ad;
+
+  this.deducerChain = [
+    this.findByAllRecipients,
+    this.findByTos,
+    this.findByAnyTo,
+    this.findByAnyCC,
+    this.guessFromTos
+  ]
 }
 
 function deductionOrNull(value, method){
@@ -18,9 +26,6 @@ function deductionOrNull(value, method){
 }
 
 LanguageDeducer.prototype = {
-  appendDeducer: function(deducer){
-    this.deducerChain.append(deducer);
-  },
   deduce: async function(){
     var context = {
       ad: this.ad,
@@ -36,43 +41,42 @@ LanguageDeducer.prototype = {
     }
     return deduction;
   },
-  deducerChain: [
-    async function(context){
-      const toandcc_key = context.ad.getKeyForRecipients(context.recipients);
-      context.ad.logger.debug("Deducing language for: " + toandcc_key);
-      const lang = await context.ad.getLangFor( toandcc_key );
-      return deductionOrNull(lang, context.ad.METHODS.REMEMBER);
-    },
-    async function(context){
-      const alltogether_key = context.ad.stringifyRecipientsGroup( context.recipients.to );
-      const lang = await context.ad.getLangFor( alltogether_key );
-      return deductionOrNull(lang, context.ad.METHODS.REMEMBER);
-    },
-    async function(context){
-      var lang = null;
-      for( const recipient of context.recipients.to ){
-        lang = await context.ad.getLangFor( recipient );
-        if( lang ){
-          break;
-        }
-      }
-      return deductionOrNull(lang, context.ad.METHODS.REMEMBER);
-    },
-    async function(context){
-      var lang = null;
-      for( const recipient of context.recipients.cc ){
-        lang = await context.ad.getLangFor( recipient );
-        if( lang ){
-          break;
-        }
-      }
-      return deductionOrNull(lang, context.ad.METHODS.REMEMBER);
-    },
-    async function(context){
-      if(await context.ad.allowHeuristic()){
-        const lang = await context.ad.heuristic_guess(context.recipients.to);
-        return deductionOrNull(lang, context.ad.METHODS.GUESS);
+
+  findByAllRecipients: async function (context) {
+    const toandcc_key = context.ad.getKeyForRecipients(context.recipients);
+    context.ad.logger.debug("Deducing language for: " + toandcc_key);
+    const lang = await context.ad.getLangFor(toandcc_key);
+    return deductionOrNull(lang, context.ad.METHODS.REMEMBER);
+  },
+  findByTos: async function(context){
+    const alltogether_key = context.ad.stringifyRecipientsGroup( context.recipients.to );
+    const lang = await context.ad.getLangFor( alltogether_key );
+    return deductionOrNull(lang, context.ad.METHODS.REMEMBER);
+  },
+  findByAnyTo: async function(context){
+    var lang = null;
+    for( const recipient of context.recipients.to ){
+      lang = await context.ad.getLangFor( recipient );
+      if( lang ){
+        break;
       }
     }
-  ]
+    return deductionOrNull(lang, context.ad.METHODS.REMEMBER);
+  },
+  findByAnyCC: async function(context){
+    var lang = null;
+    for( const recipient of context.recipients.cc ){
+      lang = await context.ad.getLangFor( recipient );
+      if( lang ){
+        break;
+      }
+    }
+    return deductionOrNull(lang, context.ad.METHODS.REMEMBER);
+  },
+  guessFromTos: async function(context){
+    if(await context.ad.allowHeuristic()){
+      const lang = await context.ad.heuristic_guess(context.recipients.to);
+      return deductionOrNull(lang, context.ad.METHODS.GUESS);
+    }
+  }
 }
