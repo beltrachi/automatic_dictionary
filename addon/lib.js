4c3cc8482b321c755922a214361aba675941d34e
Jordi Beltran <beltrachi@gmail.com>
2022-01-08 17:24:29 +0100

    Remove duplicated LoggerStub

--- addon/lib.js
+++ addon/lib.js
@@ -1,12 +1,4 @@
 export function apply(AutomaticDictionary) {
     AutomaticDictionary.Lib = {
-        // Stub to call logger when not defined
-        LoggerStub: {
-            debug: function () { },
-            performance: function () { },
-            info: function () { },
-            warn: function () { },
-            error: function () { }
-        }
     }
 }
