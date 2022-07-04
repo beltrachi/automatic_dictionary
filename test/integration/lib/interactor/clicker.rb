7a30be1312335e1d040f947c9da0d7e06ebc135e
Jordi Beltran <beltrachi@gmail.com>
2022-07-04 21:12:55 +0200

    Fixing integration tests on circleCi
    Locally tests pass but on circleCI it was almost always failing when clicking
    on the burger menu. I did not know why but after checking xdotool
    looks like its not a good idea to concat mouse move and click.
    
    Now separating the mousemove from the click executions pass.
    :fingerscrosed:

--- test/integration/lib/interactor/clicker.rb
+++ test/integration/lib/interactor/clicker.rb
@@ -6,8 +6,16 @@ module Interactor
       include Shared
 
       def click_on(position)
-        run("xdotool mousemove --sync #{position.first} #{position.last} click --clearmodifiers 1")
+        # Even if we use --sync, when using mousemove does not guarantee that
+        # cursor is in its destination. Sync only waits until it moves, not
+        # until it reaches destination.
+        # For that reason we first move the cursor with sync (to make sure it
+        # has at least moved). Then we sleep 1 sec to let cursor arrive, and then
+        # click there.
+        run("xdotool mousemove --sync #{position.first} #{position.last}")
         sleep 1
+        run('xdotool click --clearmodifiers 1')
+        sleep 0.5
       end
     end
   end
