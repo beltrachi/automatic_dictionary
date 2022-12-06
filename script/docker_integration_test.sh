25ccd14f8a87615e7d6b314259d32b2995bb7f83
Jordi Beltran <beltrachi@gmail.com>
2022-12-06 19:03:27 +0100

    CI - Recreate docker image daily to have latest thunderbird

--- script/docker_integration_test.sh
+++ script/docker_integration_test.sh
@@ -2,11 +2,14 @@
 
 set -ex
 
-docker image inspect automatic_dictionary:${THUNDERBIRD_VERSION} ||
-  docker build --pull=true --no-cache --build-arg THUNDERBIRD_VERSION=$THUNDERBIRD_VERSION -t automatic_dictionary:${THUNDERBIRD_VERSION} .
+# Recreate daily to get latest thunderbird app
+DOCKER_IMAGE="automatic_dictionary:${THUNDERBIRD_VERSION}-$(date -I)"
+
+docker image inspect $DOCKER_IMAGE ||
+  docker build --pull=true --no-cache --build-arg THUNDERBIRD_VERSION=$THUNDERBIRD_VERSION -t $DOCKER_IMAGE .
 
 if [ "$DEVEL_MODE" = "1" ]; then
    DEVEL_MODE_MODIFIERS="-v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY -e DEBUG=1 -e LOCAL_DEBUG=1 -v $PWD:/app "
 fi
 
-docker run --cap-add=SYS_PTRACE --cpus 2 -ti $DEVEL_MODE_MODIFIERS automatic_dictionary:${THUNDERBIRD_VERSION} /bin/bash -l -c "./integration_test.sh"
+docker run --cap-add=SYS_PTRACE --cpus 2 -ti $DEVEL_MODE_MODIFIERS $DOCKER_IMAGE /bin/bash -l -c "./integration_test.sh"
