8cf187600d860828b88343ba49f9ae4dadd16de8
Jordi Beltran <beltrachi@gmail.com>
2023-03-12 11:31:27 +0100

    Fix test dependencies installation of tesseract-ocr
    New base image debian 10 requires to accept unsigned packages.

--- script/install_dependencies.sh
+++ script/install_dependencies.sh
@@ -13,7 +13,7 @@ echo "deb https://notesalexp.org/tesseract-ocr5/$(lsb_release -cs)/ $(lsb_releas
 sudo apt-get update -oAcquire::AllowInsecureRepositories=true
 sudo apt-get install -y --allow-unauthenticated notesalexp-keyring -oAcquire::AllowInsecureRepositories=true
 sudo apt-get update
-sudo apt-get install -y tesseract-ocr
+sudo apt-get install -y tesseract-ocr --allow-unauthenticated
 
 sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev \
   libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev \
