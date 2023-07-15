2d8cbc3fc684c4259c76736b4933593685f31144
Jordi Beltran <beltrachi@gmail.com>
2023-07-15 12:34:11 +0200

    CI - force download latest TB image even if base image did not change

--- script/docker_integration_test.sh
+++ script/docker_integration_test.sh
@@ -6,7 +6,7 @@ set -ex
 DOCKER_IMAGE="automatic_dictionary:${THUNDERBIRD_VERSION}-$(date -I)"
 
 docker image inspect $DOCKER_IMAGE ||
-  docker build --pull=true --build-arg THUNDERBIRD_VERSION=$THUNDERBIRD_VERSION -t $DOCKER_IMAGE .
+  docker build --pull=true --no-cache --build-arg THUNDERBIRD_VERSION=$THUNDERBIRD_VERSION -t $DOCKER_IMAGE .
 
 if [ "$DEVEL_MODE" = "1" ]; then
    DEVEL_MODE_MODIFIERS="-v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY -e DEBUG=1 -e LOCAL_DEBUG=1 -v $PWD:/app "
