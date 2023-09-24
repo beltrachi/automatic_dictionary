106445f0718a83b3012f7c30a9902649fd71ba39
Jordi Beltran <beltrachi@gmail.com>
2023-09-24 21:36:43 +0200

    Fixing pre-commit

--- .github/workflows/pre-commit.cjs
+++ .github/workflows/pre-commit.cjs
@@ -1,13 +1,10 @@
 const { execSync } = require('child_process');
-const core = require('@actions/core');
 
 exports.preCommit = (props) => {
-  core.startGroup(`Running \`build.sh\``);
   execSync(
     './build.sh', {
       stdio: ['inherit', 'inherit', 'inherit'],
       encoding: 'utf-8'
     }
   );
-  core.endGroup();
 };
