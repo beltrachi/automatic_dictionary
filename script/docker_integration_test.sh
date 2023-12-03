b90e5834174ff747dd35f1ca72f7f648086494a1
Jordi Beltran <beltrachi@gmail.com>
2023-12-03 23:23:05 +0100

    chore: run container with -it on devel mode

--- script/docker_integration_test.sh
+++ script/docker_integration_test.sh
@@ -9,7 +9,7 @@ docker image inspect $DOCKER_IMAGE ||
   docker build --pull=true --build-arg THUNDERBIRD_VERSION=$THUNDERBIRD_VERSION -t $DOCKER_IMAGE .
 
 if [ "$DEVEL_MODE" = "1" ]; then
-   DEVEL_MODE_MODIFIERS="-v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY -e DEBUG=1 -e LOCAL_DEBUG=1 -v $PWD:/app "
+   DEVEL_MODE_MODIFIERS="-it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY -e DEBUG=1 -e LOCAL_DEBUG=1 -v $PWD:/app "
 fi
 
 docker run --cap-add=SYS_PTRACE --cpus 2 $DEVEL_MODE_MODIFIERS $DOCKER_IMAGE /bin/bash -l -c "./integration_test.sh"
