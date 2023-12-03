13a8408c7ae12c6ee839542ba442cfcdf7516d64
Jordi Beltran <beltrachi@gmail.com>
2023-12-03 22:23:41 +0100

    chore: move thunderbird dependencies installation to install_dependencies
    It will improve image build times by caching that layer

--- script/install_thunderbird.sh
+++ script/install_thunderbird.sh
@@ -5,9 +5,6 @@ set -ex
 # URL returns the right file for "beta-latest" or "latest", or even a specific version "78.11.0"
 PACKAGE_URL="https://download.mozilla.org/?product=thunderbird-${THUNDERBIRD_VERSION}-SSL&os=linux64&lang=en-US"
 
-# Using latest to have latest dependencies
-sudo apt-get install -y software-properties-common
-apt-get install -y `apt-cache depends thunderbird | awk '/Depends:/{print$2}'`
 curl -L $PACKAGE_URL -o thunderbird.tar.bz2
 tar -xvf thunderbird.tar.bz2 -C /tmp/
 echo "export PATH=/tmp/thunderbird/:\$PATH" >> ~/.bash_profile
