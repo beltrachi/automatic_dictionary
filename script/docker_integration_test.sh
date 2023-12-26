3b9f7c6ae3b3fa678992d2366a56521dbc6098e6
Jordi Beltran <beltrachi@gmail.com>
2023-12-27 00:17:45 +0100

    chore: always build docker image on CI
    docker cache makes it ok to build every time, its fast enough
    and don't force you onto forcing the build everytime you touch
    a setup script

--- script/docker_integration_test.sh
+++ script/docker_integration_test.sh
@@ -5,8 +5,7 @@ set -ex
 # Recreate daily to get latest thunderbird app
 DOCKER_IMAGE="automatic_dictionary:${THUNDERBIRD_VERSION}-$(date -I)"
 
-docker image inspect $DOCKER_IMAGE ||
-  docker build --pull=true --build-arg THUNDERBIRD_VERSION=$THUNDERBIRD_VERSION -t $DOCKER_IMAGE .
+docker build --pull=true --build-arg THUNDERBIRD_VERSION=$THUNDERBIRD_VERSION -t $DOCKER_IMAGE .
 
 if [ "$DEVEL_MODE" = "1" ]; then
    DEVEL_MODE_MODIFIERS="-it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY -e DEBUG=1 -e LOCAL_DEBUG=1 -v $PWD:/app "
