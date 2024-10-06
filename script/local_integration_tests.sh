1507440ff99f90455e5fb133088fe4f3fb2d55d5
Jordi Beltran <beltrachi@gmail.com>
2024-10-06 18:37:25 +0200

    feat(ci): Adding Extended Support Release version tests

--- script/local_integration_tests.sh
+++ script/local_integration_tests.sh
@@ -6,6 +6,8 @@ xhost +local:root
 
 export DEVEL_MODE=1
 
+THUNDERBIRD_VERSION=esr ./script/docker_integration_test.sh
+
 THUNDERBIRD_VERSION=latest ./script/docker_integration_test.sh
 
 THUNDERBIRD_VERSION="beta-latest" ./script/docker_integration_test.sh
