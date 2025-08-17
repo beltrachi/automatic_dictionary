1ee0c24fd35e51efa445460ef359f0f595647eaf
Jordi Beltran <beltrachi@gmail.com>
2025-08-17 15:59:13 +0200

    chore(ci): Fix tests on debian dixie (latest)

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
 # Container to run functional tests of the extension.
-FROM ruby:3.4.5 as base
+FROM ruby:3.4.5 AS base
 
 # Configure locale as utf to avoid encoding issues
 RUN apt-get update && apt-get install -y sudo
-ENV LANG C.UTF-8
-ENV LC_ALL C.UTF-8
+ENV LANG=C.UTF-8
+ENV LC_ALL=C.UTF-8
 
-ENV APP_HOME /app
+ENV APP_HOME=/app
 RUN mkdir $APP_HOME
 WORKDIR $APP_HOME
 
--- script/install_dependencies.sh
+++ script/install_dependencies.sh
@@ -6,21 +6,14 @@ sudo apt-get update
 sudo apt-get install -y xvfb zip unzip fluxbox xserver-xephyr curl \
  imagemagick libmagickwand-dev xvfb unzip xdotool
 
-# Tesseract 5
-sudo apt-get install -y apt-transport-https lsb-release
-echo "deb https://notesalexp.org/tesseract-ocr5/$(lsb_release -cs)/ $(lsb_release -cs) main" \
-| sudo tee /etc/apt/sources.list.d/notesalexp.list > /dev/null
-sudo apt-get update -oAcquire::AllowInsecureRepositories=true
-sudo apt-get install -y --allow-unauthenticated notesalexp-keyring -oAcquire::AllowInsecureRepositories=true
-sudo apt-get update
-sudo apt-get install -y tesseract-ocr --allow-unauthenticated
+# Install Tesseract OCR from default repositories
+sudo apt-get install -y tesseract-ocr tesseract-ocr-eng
 
 sudo apt-get install -y git-core curl zlib1g-dev build-essential libssl-dev \
   libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev \
   libcurl4-openssl-dev libffi-dev dbus-x11
 
 # Install thunderbird dependencies
-sudo apt-get install -y software-properties-common
 apt-get install -y `apt-cache depends thunderbird | awk '/Depends:/{print$2}'`
 
 PREV_PATH=$(pwd)
