0f2f31087d90555c44a0234df8f40e438b555e91
Jordi Beltran <beltrachi@gmail.com>
2025-08-17 15:59:51 +0200

    chore(ci): Thunderbird now distributes apps in .tar.xz

--- script/install_thunderbird.sh
+++ script/install_thunderbird.sh
@@ -10,8 +10,15 @@ if [ "$THUNDERBIRD_VERSION" == "esr" ]; then
 fi
 
 # URL returns the right file for "beta-latest" or "latest", or even a specific version "78.11.0"
+# Note: Mozilla now uses .tar.xz format instead of .tar.bz2
 PACKAGE_URL="https://download.mozilla.org/?product=thunderbird-${THUNDERBIRD_VERSION}-SSL&os=linux64&lang=en-US"
 
-curl -L $PACKAGE_URL -o thunderbird.tar.bz2
-tar -xvf thunderbird.tar.bz2 -C /tmp/
+# Install xz-utils for extracting .tar.xz files
+sudo apt-get update && sudo apt-get install -y xz-utils
+
+DOWNLOAD_DIR="/tmp/thunderbird-download"
+mkdir -p $DOWNLOAD_DIR
+cd $DOWNLOAD_DIR
+wget --content-disposition $PACKAGE_URL
+tar -xvf /tmp/thunderbird-download/* -C /tmp/
 echo "export PATH=/tmp/thunderbird/:\$PATH" >> ~/.bash_profile
